83f68748253d414ca5d69d256926540fb89980fd
schnaider30-programmer <sjeanlouis@byupathway.edu>
2025-09-16 20:43:48 -0400

    Encapsulation practice

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,46 @@
+using System;
+
+public class Fraction
+{
+    int _top;
+    int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int WholeNumber)
+    {
+        _top = WholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {
+        return $"{_top} / {_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        return  (double) _top / (double) _bottom ;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetTop());
+        Console.WriteLine(f2.GetBottom());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetTop());
+        Console.WriteLine(f3.GetBottom());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetTop());
+        Console.WriteLine(f4.GetBottom());
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        f2.SetTop(9);
+        Console.WriteLine(f2.GetTop());
+
+        f3.SetBottom(10);
+        Console.WriteLine(f3.GetBottom());
+
+
     }
 }
